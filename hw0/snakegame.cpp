eb014aaed846d287e5b88da524869d2042e97495
affiq23 <affiqem@gmail.com>
2023-02-05 17:59:55 -0600

    added tail feature

--- hw0/snakegame.cpp
+++ hw0/snakegame.cpp
@@ -5,29 +5,36 @@ DATE LAST MODIFIED: 02/5/2023
 CLASS SECTION: CS 1337.012
 PURPOSE: A simple game that uses WASD keys to control a snake on screen that can eat a piece of fruit to
 keep growing; eating more fruit increases score
-CHANGELOG: 
+CHANGELOG:
 01/25/23: renamed functions to more accurately describe role in code
           added brackets to if statements and for loops
           added first bracket of function to same line declararation to use less lines
           added uppercase keys to switch statements as well (kept clicking caps lock and game wouldn't
           let me turn snake in time)
-
 */
 
 #include <iostream>
 #include <ncurses.h>
 using namespace std;
 bool gameOver;
+bool ranIntoTail;
 const int width = 20;
 const int height = 20;
 int x, y, fruitXPos, fruitYPos, score;
 int tailX[100], tailY[100];
 int nTail;
-enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN};
+enum eDirecton
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
 eDirecton dir;
 
-
-void setupBoard(){
+void setupBoard()
+{
     gameOver = false;
     dir = STOP;
     x = width / 2;
@@ -40,48 +47,59 @@ void setupBoard(){
     timeout(100);
 }
 
-void drawSnake(){
-    system("clear"); //system("cls");
-    for (int i = 0; i < width+2; i++){
+void drawSnake()
+{
+    system("clear");
+    for (int i = 0; i < width + 2; i++)
+    {
         cout << "#";
     }
 
     cout << "\r" << endl;
- 
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < width; j++){
-            if (j == 0){
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+            {
                 cout << "#";
             }
 
-            if (i == y && j == x){
-                cout << "O"; //print snake head
+            if (i == y && j == x)
+            {
+                cout << "O"; // print snake head
             }
-            else if (i == fruitYPos && j == fruitXPos){
-                cout << "F"; //printing fruit
+            else if (i == fruitYPos && j == fruitXPos)
+            {
+                cout << "F"; // printing fruit
             }
-            else{
+            else
+            {
                 bool print = false;
-                for (int k = 0; k < nTail; k++){
-                    if (tailX[k] == j && tailY[k] == i){
-                        cout << "o"; //printing additional bodies to snake if fruit is eaten
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o"; // printing additional bodies to snake if fruit is eaten
                         print = true;
                     }
                 }
 
-                if (!print){
+                if (!print)
+                {
                     cout << " ";
                 }
             }
-                 
- 
+
             if (j == width - 1)
                 cout << "#";
         }
         cout << "\r" << endl;
     }
- 
-    for (int i = 0; i < width+2; i++){
+
+    for (int i = 0; i < width + 2; i++)
+    {
         cout << "#";
     }
 
@@ -89,11 +107,12 @@ void drawSnake(){
     cout << "Score:" << score << "\r" << endl;
 }
 
-void userInput(){
-    //switch case gets the specific key user inputs to move the snake
-    switch (getch())
-    {
-    case 'a':
+// switch case gets the specific key user inputs to move the snake
+void userInput()
+{
+
+    switch (getch()){
+    case 'a': // added support for both uppercase and lowercase keyboard input
         dir = LEFT;
         break;
     case 'd':
@@ -159,60 +178,101 @@ void snakeLogic()
         break;
     }
 
-    if(x >= width){
+    if (x >= width)
+    {
         x = 0;
-    } 
-    else if (x < 0){
+    }
+    else if (x < 0)
+    {
         x = width - 1;
-     }
+    }
 
-    if(y >= height){
+    if (y >= height)
+    {
         y = 0;
-    }  
-    else if (y < 0){
-     y = height - 1;
     }
- 
-    for (int i = 0; i < nTail; i++){
-        if (tailX[i] == x && tailY[i] == y){
+    else if (y < 0)
+    {
+        y = height - 1;
+    }
+
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+        {
             gameOver = true;
+            ranIntoTail = true;
         }
-            
     }
- 
-    if(x == fruitXPos && y == fruitYPos){
+
+    if (x == fruitXPos && y == fruitYPos)
+    {
         score += 10;
         fruitXPos = rand() % width;
         fruitYPos = rand() % height;
         nTail++;
     }
 }
-int main()
+
+// displays at the beginning of the program when you run for the first time
+string instructions()
 {
-
     string answer;
-
     cout << "Welcome to Snake Game! The objective is to eat the fruit pieces and avoid running into yourself.\n";
     cout << "Would you like to play?\n";
     cout << "Y or N" << endl;
     cin >> answer;
+    return answer;
+}
 
+bool playAgain()
+{
+    string answer;
+    cout << "Would you like to play again?\n";
+    cout << "Y or N" << endl;
+    cin >> answer;
 
-    if(answer == "Y"){
-        
-        setupBoard();
+    if (answer == "Y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
-    while (!gameOver){
+// took the functions in the main function and put them in their own function so they can be called in a more efficient manner
+void playGame()
+{
+    setupBoard();
+
+    while (!gameOver)
+    {
         drawSnake();
         userInput();
         snakeLogic();
     }
     endwin();
-    return 0;
+}
+
+int main()
+{
+
+    bool isValid = true;
+
+    if (instructions() == "Y"){ // checking if the user wants to play or not
+        playGame();
+
+        if (ranIntoTail)
+        {
+            cout << "You ran into your tail!";
+        }
+        else
+        {
+            cout << "You didn't run into your tail!";
+        }
     }
 
-    else{
-        cout << "No problem!";
-    }
-    
+    return 0;
 }
