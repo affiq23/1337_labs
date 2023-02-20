1fbcc69e55e351b25f21a9712fa86db000b82e92
affiq23 <affiqem@gmail.com>
2023-02-19 23:24:48 -0600

    push all code

--- hw2/properSearch.cpp
+++ hw2/properSearch.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -41,52 +42,42 @@ void checkAndOpenFile(){
 
 }
 
-// Searches the array and returns true if found
-bool search2D(char *grid, int row, int col, string word, int R, int C, int &direction)
-{
-    // If first character of word doesn't
-    // match with given starting point in grid.
-    if (*(grid + row * C + col) != word[0])
-    {
-        // cout << "Nothing at " << row << " " << col << endl;
+//searches array for word
+bool search2D(char *grid, int row, int col, string word, int R, int C, int &direction){
+    
+    if (*(grid + row * C + col) != word[0]){ //if the first character doesn't match starting point
         return false;
     }
-     int length = word.length();
-    // cout << "len is " << len << endl;
 
-    // Search word in all 8 directions
-    // starting from (row, col)
-    for (int dir = 0; dir < 8; dir++)
-    {
+     int length = word.length();
+
+    //search from [row][col]
+    for (int dir = 0; dir < 8; dir++) {
      
      //initialize starting point
         int checkingLen;
-        int rd = row + x[dir];
-        int cd = col + y[dir];
-
-     
+        int startrow = row + x[dir];
+        int startcol = col + y[dir];
 
        //check if remaining characters match
-        for (checkingLen = 1; checkingLen < length; checkingLen++)
-        {
-            // cout << "k: " << k << endl;
-            //  If out of bound break
-            if (rd >= R || rd < 0 || cd >= C || cd < 0)
+        for (checkingLen = 1; checkingLen < length; checkingLen++){
+            //break if out of bounds
+            if (startrow >= R || startrow < 0 || startcol >= C || startcol < 0){
                 break;
+            }
 
-            // If not matched,  break
-            if (*(grid + rd * C + cd) != word[checkingLen])
+           //if no match
+            if (*(grid + startrow * C + startcol) != word[checkingLen]){
                 break;
+             }
 
-            // Moving in particular direction
-            rd += x[dir];
-            cd += y[dir];
+            startrow += x[dir];
+            startcol += y[dir];
             
         }
 
-        // If all character matched, then value of k equals the length of the word
-        if (checkingLen == length)
-        {
+        //checkingLen should equal len if all characters match
+        if (checkingLen == length){
             direction = dir;
             return true;
         }
@@ -94,39 +85,33 @@ bool search2D(char *grid, int row, int col, string word, int R, int C, int &dire
     return false;
 }
 
-// Searches given word in a given
-// matrix in all 8 directions
-int patternSearch(char *grid, string wordNoSpace, string wordSpace, int R, int C, int &dir)
-{
+
+//searches for word in all directions
+int patternSearch(char *grid, string wordNoSpace, string wordSpace, int R, int C, int &direction){
     //  every point start
     // point and search words
-    for (int row = 0; row < R; row++)
-    {
-        for (int col = 0; col < C; col++)
-        {
-            if (search2D(grid, row, col, wordNoSpace, R, C, dir))
-            {
-                if (dir != 0)
-                    cout << wordNoSpace << " found at " << row + 1<< ", " << col + 1<< ": "; //incrementing row and col by one to get rid of zero-based index
+    for (int row = 0; row < R; row++){
+        for (int col = 0; col < C; col++){
+            if (search2D(grid, row, col, wordNoSpace, R, C, direction)){
+                if (direction != 0){
+                    cout << wordNoSpace << " found at " << row++<< ", " << col++<< ": "; //get rid of 0 based index
+                }
             }
         }
     }
-    return dir;
+    return direction;
 }
 
 
 int main(){
 
-   
     checkAndOpenFile(); //checks and opens file
 
-
-    // Get rows and columns
+    //get rows, cols
     while (!file.eof())
     {
         getline(file, line);
-        if (line[0] != '#' && line.length() != 0 && line.length() != 1)
-        {
+        if (line[0] != '#' && line.length() != 0 && line.length() != 1){
 
             stringstream s(line);
             s >> row >> col;
@@ -135,19 +120,14 @@ int main(){
     }
     cout << "Nbr Rows: " << row << ";  Nbr Cols: " << col << endl;
 
-    // Create matrix
+    // create matrix
     char matrix[row][col];
-    while (true)
-    {
+    while (true){
         getline(file, line);
-        if (line[0] != '#' && line.length() != 0 && line.length() != 1)
-        {
-            for (int i = 0; i < row; i++)
-            {
-                if (line[0] != '#' && line.length() != 0 && line.length() != 1)
-                {
-                    for (int j = 0; j < line.length(); j++)
-                    {
+        if (line[0] != '#' && line.length() != 0 && line.length() != 1) { //checking if character is not # and exists
+            for (int i = 0; i < row; i++) {
+                if (line[0] != '#' && line.length() != 0 && line.length() != 1){ 
+                    for (int j = 0; j < line.length(); j++){
                         matrix[i][j] = line[j];
                     }
                 }
@@ -158,38 +138,28 @@ int main(){
     }
 
     // display matrix
-    for (int i = 0; i < row; i++)
-    {
+    for (int i = 0; i < row; i++){
         for (int j = 0; j < col; j++)
             cout << matrix[i][j];
-
         cout << endl;
     }
 
-    // get the words
-    
-
-    while (!file.eof())
-    {
+    // get words from file
+    while (!file.eof()){
         getline(file, line);
-        if (line[0] != '#' && !isspace(line[0]))
-        {
+        if (line[0] != '#' && !isspace(line[0])){
             words.push_back(line);
         }
     }
 
     // get rid of the empty vectors
-    for (int i = 0; i < words.size(); i++)
-    {
-        if (words[i].length() == 0)
-        {
+    for (int i = 0; i < words.size(); i++){
+        if (words[i].length() == 0) {
             words.erase(words.begin() + i);
         }
     }
 
     // make a copy of words with all capital letters
-   
-
     for (int i = 0; i < words.size(); i++){
         string delimiter = "\n";
         string line1 = words[i];
@@ -197,9 +167,9 @@ int main(){
         c = toupper(static_cast<unsigned char>(c));
     }
 
-        //removing the \n occurence
-        size_t pos = 0;
-         while ((pos = line1.find(delimiter)) != std::string::npos) {
+    //removing the \n occurence
+    size_t pos = 0;
+    while ((pos = line1.find(delimiter)) != std::string::npos) {
         line1.erase(pos, delimiter.length());
     }
 
@@ -211,52 +181,58 @@ int main(){
     for (int i = 0; i < words.size(); i++)
     {
        for (const auto& word : words) {
-        std::string temp;
+        string tempString;
         for (char c : word) {
             if (!std::isspace(c)) {
-                temp += std::toupper(c);
+                tempString += toupper(c);
             }
         }
-        noSpaceWords.push_back(temp);
+        noSpaceWords.push_back(tempString);
         }
     }
 
     cout << endl;
 
-    // search and get directions
-    for (int i = 0; i < words.size(); i++)
-    {
+    // search and get directions one by one
+    for (int i = 0; i < words.size(); i++){
         int direction = 0;
         direction = patternSearch((char *)matrix, noSpaceWords[i], words[i], row, col, direction);
-        if (direction == 0)
+        if (direction == 0){
             wordsNotFound.push_back(words[i]);
-        else if (direction == 1)
+        }
+        else if (direction == 1){
             cout << "(direction = N)" << endl;
-        else if (direction == 2)
+        }
+        else if (direction == 2){
             cout << "(direction = NE)" << endl;
-        else if (direction == 3)
+        }
+       else  if (direction == 3){
             cout << "(direction = W)" << endl;
-        else if (direction == 4)
+        }
+        else if (direction == 4){
             cout << "(direction = E)" << endl;
-        else if (direction == 5)
+        }
+       else  if (direction == 5){
             cout << "(direction = SW)" << endl;
-        else if (direction == 6)
+        }
+       else if (direction == 6){
             cout << "(direction = S)" << endl;
-        else if (direction == 7)
+        }
+       else  if (direction == 7){
             cout << "(direction = SE)" << endl;
-        else if (direction == 8)
+        }
+        else if (direction == 8){
             cout << "(direction = NW)" << endl;
-        else
+        }
+        else{
             wordsNotFound.push_back(words[i]);
+        }
     }
 
-    cout << endl
-         << "Couldn't find these movies:" << endl;
-    for (int i = 0; i < wordsNotFound.size(); i++)
-    {
+    cout << endl << "Couldn't find these movies:" << endl; //returns movies not found
+    for (int i = 0; i < wordsNotFound.size(); i++){
         cout << wordsNotFound[i] << endl;
     }
 
     return 0;
 }
-
