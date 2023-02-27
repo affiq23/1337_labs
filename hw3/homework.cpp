9457c4c643ce373c905418a74a4ef51946936d8e
affiq23 <affiqem@gmail.com>
2023-02-27 14:40:14 -0600

    added sort and copy of vectorOne

--- hw3/homework.cpp
+++ hw3/homework.cpp
@@ -3,57 +3,81 @@
                 Wrote generateVector() function, which takes input from user and creates vector
                 Have to implement boundary check if largestNum is greater than 20
 
+- 2/27/23:      Wrote showVector function to display contents and vector and size
+                Created new copy of vectorOne
+                Was able to sort vectorOne (original vector) using sort algorithm and display it
 */
-
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-
-void show(string msg){
-    cout << "SHOWING: " << msg << endl; 
+void show(string msg)
+{
+    cout << "SHOWING: " << msg << endl;
 }
 
- vector <int> generateVector(){
+vector<int> generateVector()
+{
     int sizeVec;
     int largestNum;
-    vector <int> vecOne;
+    vector<int> vecOne;
 
     show("Called generateVector");
     cout << "What size vector do you want?" << endl;
     cin >> sizeVec;
     cout << "What is the largest value number you want generated?" << endl;
     cin >> largestNum;
-    
-    while(largestNum > 20){
-        if(largestNum> 20){
+
+    while (largestNum > 20)
+    {
+        if (largestNum > 20)
+        {
             cout << "Sorry. Pick another number less than 20. ";
             cin >> largestNum;
         }
     }
 
-
-    for(int i = 0; i < sizeVec; i++){
+    for (int i = 0; i < sizeVec; i++)
+    {
         int j = (rand() % largestNum) + 1;
         vecOne.push_back(j);
     }
     show("Populated generateVector");
 
     return vecOne;
-
 }
 
-int main(){
-
-vector <int> vectorOne = generateVector();
-
-for(int i = 0; i < vectorOne.size(); i++){
-    cout << vectorOne[i] << "|| ";
+void showVector(vector<int> vector, string msg)
+{
+    cout << msg << endl;
+    for (int i = 0; i < vector.size(); i++)
+    {
+        cout << vector[i] << "| ";
+    }
+    cout << endl;
+    cout << "Vector size: " << vector.size() << endl;
+    cout << "Vector capacity: " << vector.capacity() << endl;
 }
 
-cout << endl;
+vector<int> copyVector(vector<int> orgVector)
+{
+    vector<int> vectorTwo = orgVector;
+    return vectorTwo;
+}
 
-return 0;
+int main()
+{
 
+    vector<int> vectorOne = generateVector(); //creates vector
+
+    vector <int> vectorTwo = copyVector(vectorOne); //creates copy of original vector
+    showVector(vectorOne, "print vector unsorted");
+    
+    sort(vectorOne.begin(), vectorOne.end());
+    showVector(vectorOne, "print vector sorted");
+    showVector(vectorTwo, "print vector new");
+
+    return 0;
 }
